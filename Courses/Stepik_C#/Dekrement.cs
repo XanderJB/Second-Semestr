d6e807a03091ffc5ea326fcd5b062d8a7b4b152a
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2022-01-17 12:40:05 +0300

    Create Dekrement.cs

---
+++ Courses/Stepik_C#/Dekrement.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class MainClass
+{
+     static void Main(string[] args)
+        {
+            int num_1 = 0;
+            int num_2 = 4;
+            int res = --num_1 * num_2;
+            Console.WriteLine(res);
+        }
+}
