415eaf402158c0afc1a6a148921e2531b6eb842b
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2022-01-25 10:44:41 +0300

    Create cycle_for.cs

---
+++ Courses/Stepik_C#/cycle_for.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+       static void Main(string[] args)
+        {
+            int count_1 = int.Parse(Console.ReadLine());
+            for (int  count =1; count<=count_1;count++)
+            {
+                Console.WriteLine("Ошибка!");
+            }
+        }
+}
