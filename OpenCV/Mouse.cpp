becb13fe242d4e2cc61eca39d43e6a5cd8c93615
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2022-02-14 10:28:22 +0300

    Update Mouse.cpp

--- OpenCV/Mouse.cpp
+++ OpenCV/Mouse.cpp
@@ -1,98 +1,160 @@
 // Защита OpenCV
-// 1. Что значит строка unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
-// 2. Какие существуют потоки для работы с файлами
-// 3. Дополнить код, для считывание из файла координат
-// 4. Доюавить перевод пиксельных координат в географические
+// 1. Что значит строка unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1); - отображение точек на заданных координтах
+// 2. Какие существуют потоки для работы с файлами - ifstream: для чтения с файла; ofstream: для записи в файл; fstream: совмещает запись и чтение
+// 3. Дополнить код, для считывание из файла координат - done
+// 4. Доюавить перевод пиксельных координат в географические - done
 // 5. Изменить команду, чтобы реагировало на правое нажатие мыши.
 
-//пока что есть ошибки
-#define _CRT_SECURE_NO_WARNINGS
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
 #include <chrono>
 #include <ctime>
-#include <fstream>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 #include <string>
-
+#include <cstring>
 
 using namespace cv;
 using namespace std;
 
-Mat img;
 void my_mouse_callback(int event, int x, int y, int flags, void* param);
+int reading(string Filename, string imagename, double wl, double hl, double wr, double hr);
+void file();
 
 int main(int argc, char** argv)
 {
+	// устанвливаем русскую локаль для консоли
+	setlocale(LC_ALL, "Russian");
+	//file();
 
-	const char* traectory_file = "coordinat.txt";
+	const char* imagename = "town.png";
+	const char* traectory_file = "test1.txt";
 	double wl = 55.639799;
 	double hl = 37.828428;
 	double wr = 55.622020;
 	double hr = 37.873735;
-
-	Mat image = imread("town.png");
-	cout << "Размер изображения: " << endl;
-	cout << "Ширина: " << endl;
-	cout << "Высота: " << endl;
-
-	long double koef_mashtabW, koef_mashtabH;
-	koef_mashtabW = (wl - wr) / img.size().width;
-	koef_mashtabH = (hr - hl) / img.size().height;
-
-	cout << "Ваш путь пролегает по следующему маршруту: " << endl;
-	while (!f.eof()) 
+	/*reading("test1.txt", "town.png", wl, hl, wr, hr);*/
+	cout << "Выберите какое действие вы хотите выполнить: считать или записать?" << endl;
+	int userAnswer = 0;
+	cin >> userAnswer;
+	cout << userAnswer;
+	if (userAnswer == 1) {
+		reading("test1.txt", "town.png", wl, hl, wr, hr);
+	}
+	else if (userAnswer == 2)
 	{
-		f >> tt >> xm >> ym;
-		shirota = koef_mashtabW * (img.size().width - x) + wr;
-		dolgota = koef_mashtabH * (img.size().height - y) + hl;
-		cout << "Метка времени" << t << " широта: " << shirota << " долгота: " << dolgota << endl;
+		Mat image = imread("town.png");
 
-
+		namedWindow("modernGoogle");
+		setMouseCallback("modernGoogle", my_mouse_callback, &image);
+		while (true) {
+			imshow("modernGoogle", image);
+			waitKey(30);
+		}
+		
 	}
-	namedWindow("modernGoogle");
-	setMouseCallback("modernGoogle", my_mouse_callback, &image);
-	/*Mat dst, im;*/
-	while (true) {
-		imshow("modernGoogle", image);
-		waitKey(30);
+	else {
+		return(0);
 	}
-	return(0);
+	
 }
 
-void my_mouse_callback(int event, int x, int y, int flags, void* param)
-{
-	if (event == EVENT_LBUTTONDOWN)
+	void my_mouse_callback(int event, int x, int y, int flags, void* param)
 	{
-		Mat* plmage = (Mat*)param;
-		Mat image = *plmage;
-		x = xm;
-		y = ym;
-		imshow("route", img);
-		waitKey(tt - t);
-		t = tt;
-	}
-		circle(image, Point(x, y), 5, Scalar(255, 0, 0), FILLED, 8, 0);
-		std::ofstream file;
-		file.open("test1.txt", ios_base::app);
-		unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
-		file << " Meтка времени: " << milliseconds_since_epoch <<  " широта: " << x << " " << " долгота: " << y << endl;
-		file.close();
+		if (event == EVENT_LBUTTONDOWN)
+		{
+			Mat* plmage = (Mat*)param;
+			Mat image = *plmage;
+			/*x = xm;
+			y = ym;
+			imshow("route", img);
+			waitKey(tt - t);
+			t = tt;*/
+
+			circle(image, Point(x, y), 5, Scalar(255, 0, 0), FILLED, 8, 0);
+			std::ofstream file;
+			file.open("test1.txt", ios_base::app);
+			unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
+			file << milliseconds_since_epoch << " " <<  x << " " <<   y << endl;
+			file.close();
+		}
 	
 
-	std::string line;
 
-	std::ifstream in("test1.txt"); // окрываем файл для чтения
-	if (in.is_open())
-	{
-		while (getline(in, line))
-		{
-			std::cout << line << std::endl;
-		}
+		//std::string line;
+
+		//std::ifstream in("test1.txt"); // окрываем файл для чтения
+		//if (in.is_open())
+		//{
+		//	while (getline(in, line))
+		//	{
+		//		std::cout << line << std::endl;
+		//	}
+		//}
+		//in.close();     // закрываем файл
+
+	  /*  std::cout << "End of program" << std::endl;*/
 	}
-	in.close();     // закрываем файл
 
-  /*  std::cout << "End of program" << std::endl;*/
+	int reading(string Filename, string imagename, double wl, double hl, double wr, double hr)
+	{
+
+
+
+		string File = Filename;
+		int x, y, xm, ym;
+		unsigned long t, tt;
+		long double shirota, dolgota;
+
+		ifstream f(File);
+		if (!(f.is_open()))  cout << "Ошибка открытия файла" << File;
+		else
+		{
+			Mat img = imread(imagename);
+			cout << "Размер изображения: " << endl;
+			cout << "Ширина: " << img.size().width << endl;
+			cout << "Высота: " << img.size().height << endl;
+			//вычисляем коэффициент масштабирования
+			long double koef_mashtabW, koef_mashtabH;
+			koef_mashtabW = (wl - wr) / img.size().width;
+			koef_mashtabH = (hr - hl) / img.size().height;
+			//cout << koef_mashtabW << koef_mashtabH << endl;
+
+			f >> t >> x >> y;
+
+			cout << "Ваш путь пролегает по следующему  маршруту: " << endl;
+			while (!f.eof())
+			{
+				f >> tt >> xm >> ym;
+
+				//cout << "Метка времени: " << t << " x: " << x << " y: " << y << endl;
+				shirota = koef_mashtabW * (img.size().width - x) + wr;
+				dolgota = koef_mashtabH * (img.size().height - y) + hl;
+				cout << "Метка времени: " << t << " широта: " << shirota << " долгота: " << dolgota << endl;
+
+				circle(img, Point(x, y), 5, Scalar(0, 0, 255), FILLED, 8, 0);
+				x = xm;
+				y = ym;
+				imshow("route", img);
+				waitKey(tt - t);
+				t = tt;
+			}
+		}return 0;
+	}
+void file()
+{
+	ofstream file;
+	file.open("test.txt", ios_base::app);
+	unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
+	file << milliseconds_since_epoch << endl;
+	file.close();
+
 }
+
+
