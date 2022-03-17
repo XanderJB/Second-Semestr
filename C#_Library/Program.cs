d1f432b0e8d4bc4be0def2f64eba753d10ada9d2
palmistriya <86486142+sanyagribanov@users.noreply.github.com>
2022-03-17 10:07:38 +0300

    Add files via upload

---
+++ C#_Library/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibraryTest;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int year = rnd.Next(2000, 2004);
+            int group = rnd.Next(190, 220);
+            Console.WriteLine("Напишите ФИО студента:");
+            string fio = Console.ReadLine();
+            string result = Library.GetStudNumber(year, group, fio);
+        }
+    }
+}
