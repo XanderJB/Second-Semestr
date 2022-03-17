fb3c8acb153ad77bce651339ced4273e4500c14f
palmistriya <86486142+sanyagribanov@users.noreply.github.com>
2022-03-17 13:02:04 +0300

    Add files via upload

---
+++ C#_Library/Marks/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibraryTest;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static List<Mark> GetMarks(DateTime now, List<string> students)
+        {
+            List<Mark> result = new List<Mark>();
+            return result; 
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int year = rnd.Next(2000, 2004);
+            int group = rnd.Next(190, 220);
+            /*Console.WriteLine("Напишите ФИО студента:");*/
+           /* string fio = Console.ReadLine();*/
+            /*string result = Library.GetStudNumber(year, group, fio);*/
+
+            List<string> Students = new List<string>();
+            Students.Add("Student1");
+            Students.Add("Student2");
+
+            /*DateTime now = DateTime.Now;*/
+
+            /*List<Mark> result1 = GetMarks(now, Students);*/
+            for (int i=0; i<10; i++)
+            {
+                DateTime now = DateTime.Now;
+                DateTime data = now.AddDays(i);
+                Console.WriteLine(data);
+
+/*
+                DateTime currentDate = DateTime.Now;
+                DateTime date = currentDate.AddDays(j);
+                Console.WriteLine("Day {0}: {1}", j + 1, date);
+*/
+
+                for (int j = 0; j < Students.Count; j++)
+                {
+                    
+                }
+                List<Mark> listMark = new List<Mark>();
+                Mark mark = new Mark();
+                mark.Estimation = "5";
+                mark.date = DateTime.Now;
+                mark.Student = "Алибеков Мустафа";
+                listMark.Add(mark);
+
+                listMark.ForEach(p => Console.WriteLine($"" + $"Студент: {p.Student}, Дата выставленной отметки: {p.date}, Оценка: {p.Estimation}"));
+            }
+        }
+    }
+}
---
+++ C#_Library/Marks/Students.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+/*группа, год поступления, ФИО.*/
+
+    class Students
+    {
+        int group;
+        int year;
+        string fio;
+
+    }
+
+   public class Mark
+    {
+        public string Student;
+        public DateTime date;
+        public string Estimation;
+
+        internal void ForEach(Action<object> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
