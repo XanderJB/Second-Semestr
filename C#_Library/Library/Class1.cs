0f8519f377ed59cf83cd44a9c5601846e66e03db
palmistriya <86486142+sanyagribanov@users.noreply.github.com>
2022-03-22 10:15:27 +0300

    Update Class1.cs

--- C#_Library/Library/Class1.cs
+++ C#_Library/Library/Class1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ClassLibraryTest
 {
+  
     static public class Library
     {
         static public void test_function()
@@ -38,7 +41,66 @@ namespace ClassLibraryTest
             Console.WriteLine("{0}.{1}.{2}{3}{4}", year, group, LastName[0], Name[0], FatherName[0]);
             string res = "The End";
             return res;
+        }
 
+        static public List<Mark> GetMarks(DateTime now, List<string> students)
+        {
+            List<Mark> marks = new List<Mark>();
+
+            for (int j = 0; j < 30; j++)
+            {
+
+                DateTime date = now.AddDays(j);
+                Console.WriteLine(date);
+
+                for (int i = 0; i < students.Count; i++)
+                {
+                    var random = new Random();
+                    string[] Estimation = { "5", "4", "3", "2", "прогул", "болезнь" };
+
+                    int index = random.Next(Estimation.Length);
+                    Console.WriteLine("ФИО: {0}; Оценка: {1}", students[i], Estimation[index]);
+
+                    Mark mark = new Mark();
+                    mark.Estimation = Estimation[index];
+                    mark.date = date;
+                    mark.Student = students[i];
+                    //test_mark1.Print();
+                    marks.Add(mark);
+
+                }
+
+            }
+            //marks.ForEach(p => Console.WriteLine($"" + $"Дата выставленной отметки: {p.date}\n Студент: {p.Student},  Оценка: {p.Estimation}"));
+
+            return marks;
+        }
+
+        static public int GetCountTruancy(List<Mark> marks)
+        {
+            DateTime date1 = new DateTime(2022, 03, 21, 0, 0, 0);
+            var Truancy = from p in marks where p.Estimation == "прогулы" where p.date >= date1 select p;
+            int count = 0;
+            foreach (Mark mark in Truancy)
+            {
+                Console.WriteLine($"Студент: {mark.Student}, Оценка: {mark.Estimation}");
+                count += 1;
+            }
+            Console.WriteLine($"Количество прогулов: {count}");
+            return count;
+        }
+        static public int GetCountDicease(List<Mark> marks)
+        {
+            DateTime date1 = new DateTime(2022, 03, 21, 0, 0, 0);
+            var Truancy = from p in marks where p.Estimation == "болезнь" where p.date >= date1 select p;
+            int count = 0;
+            foreach (Mark mark in Truancy)
+            {
+                Console.WriteLine($"Студент: {mark.Student}, Оценка: {mark.Estimation}");
+                count += 1;
+            }
+            Console.WriteLine($"Количество болезней: {count}");
+            return count;
         }
     }
 }
