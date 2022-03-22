d49a944775bf69113c175706b141dd6ef57e302f
palmistriya <86486142+sanyagribanov@users.noreply.github.com>
2022-03-22 10:14:59 +0300

    Update Program.cs

--- C#_Library/Program.cs
+++ C#_Library/Program.cs
@@ -1,19 +1,33 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using ClassLibraryTest;
 
-namespace ConsoleAppTest
+namespace Popytka3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Students student_1 = new Students();
+
+            DateTime now = DateTime.Now;
+
             Random rnd = new Random();
 
-            int year = rnd.Next(2000, 2004);
-            int group = rnd.Next(190, 220);
-            Console.WriteLine("Напишите ФИО студента:");
-            string fio = Console.ReadLine();
-            string result = Library.GetStudNumber(year, group, fio);
+            student_1.year = rnd.Next(2000, 2004);
+            student_1.group = rnd.Next(190, 220);
+            student_1.fio = ("Алибеков Мустафа");
+
+            List<string> Students = new List<string>();
+            Students.Add(student_1.fio);
+            Students.Add("Михаил Калашников");
+
+
+            List<Mark> res = Library.GetMarks(now, Students);
+
+            int res_1 = Library.GetCountTruancy(res);
+            int res_2 = Library.GetCountDicease(res);
+
         }
     }
 }
